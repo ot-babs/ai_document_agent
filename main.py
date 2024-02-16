75fefc3fc4261fa77c28b7dd8597d0409a13adea
ot-babs <106931726+ot-babs@users.noreply.github.com>
2024-02-16 15:06:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import streamlit as st
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+
+
+def load_document(file):
+    import os
+    name, extension = os.path.splitext(file)
+
+    if extension == '.pdf':
+
+        from langchain.document_loaders import PyPDFLoader
+        print(f'Loading {file}')
+        loader = PyPDFLoader(file)
+
+    elif extension == '.docx':
+        from langchain.document_loaders import Docx2txtLoader
+        print(f'Loading {file}')
+        loader = Docx2txtLoader(file)
+    elif extension == '.txt':
+        from langchain.document_loaders import TextLoader
+        loader = TextLoader(file)
+    else:
+        print('Document format is not supported.')
+        return None
+
+    data = loader.load()
+    return data
+
+
+def chunk_data(data, chunk_size=256, chunk_overlap=20):
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    chunks = text_splitter.split_documents(data)
+    return chunks
+
+
+def create_embeddings(chunks):
+    embeddings = OpenAIEmbeddings()
+    vectore_store = Chroma.from_dcouments(chunks, embeddings)
+    return vectore_store
+
+
+def q_and_a(vector_store, q, k=3):
+    from langchain.chains import RetrievalQA
+    from langchain_openai import ChatOpenAI
+
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=1)
+
+    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': k})
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+    answer = chain.run(q)
+    return answer
+
+
+def calculate_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model('text-embedding-ada-002')
+    total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
+    price = {total_tokens / 1000 * 0.0004:.6}
+    # print(f'Total Tokens: {total_tokens}')
+    # print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.0004:.6f}')
+    return total_tokens, price
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv(), override=True)
+
+    st.image('img.png')
+    st.subheader('LLM Question-Answering Application ')
+    with st.sidebar:
+        api_key = st.text_input('OpenAI API key:', type='password')
+        if api_key:
+            os.environ['OPENAI_API_KEY'] = api_key
+
+        uploaded_file = st.file_uploader('Upload a file', type=['pdf', 'docx', 'txt'])
+        chunk_size = st.number_input('Chunk')
+
