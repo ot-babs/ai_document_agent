624ca728b704574b059fa90f2237dd9bb6eed7ac
ot-babs <106931726+ot-babs@users.noreply.github.com>
2024-02-16 15:10:30 +0000

    Connected frontend streamlit to backend of Ai agent

--- main.py
+++ main.py
@@ -77,5 +77,7 @@ if __name__ == "__main__":
             os.environ['OPENAI_API_KEY'] = api_key
 
         uploaded_file = st.file_uploader('Upload a file', type=['pdf', 'docx', 'txt'])
-        chunk_size = st.number_input('Chunk')
+        chunk_size = st.number_input('Chunk size:', min_value=100, max_value=2048, value=512)
+        k = st.number_input('k', min_value=1, max_value=20, value=3)
+        add_data = st.button('Add Data')
 
